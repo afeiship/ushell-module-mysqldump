eed941c767865836789c483c1be8e2c7e0296ee0
feizheng <1290657123@qq.com>
2016-11-30 10:25:20 +0800

    remove unused code

--- index.sh
+++ index.sh
@@ -5,8 +5,4 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 source $ROOT_PATH/functions.sh;
 
-alias usm_exportdb_structure='usm_mysql_export_structure';
-alias usm_exportdb_data='usm_mysql_export_data';
-alias usm_exportdb_all='usm_mysql_export_all';
-
 unset ROOT_PATH;
